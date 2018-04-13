5dc7072cd6ed2ff0e3deb50b942bde9ae3cfe6cf
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-04-13 15:32:23 +0200

    Add Broker Layout and add route to component

---
+++ src/layouts/broker/index.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Row, Col, Button, Table, Input } from 'antd';
+import { connect } from 'react-redux';
+import MainLayout from '../main/MainLayout'
+import CreateCertificate from '../../components/create_certificate/CreateCertificate'
+
+const columns = [
+  {
+    title: 'Owner',
+    dataIndex: 'owner',
+    key: 'owner',
+    className: 'table__column'
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+    className: 'table__column'
+  },
+  {
+    title: 'Certificate Number',
+    dataIndex: 'certificate_number',
+    key: 'certificate_number',
+    className: 'table__column'
+  },
+  {
+    title: 'Effective Date',
+    dataIndex: 'effective_date',
+    key: 'effective_date',
+    className: 'table__column'
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+        <a href="#">View details</a>
+      </span>
+    ),
+    className: 'table__column'
+  }
+];
+
+class BrokerIndex extends Component {
+  state = {
+    loading: false,
+    visible: false
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    });
+  }
+
+  handleOk = (certificate) => {
+    this.setState({ loading: true });
+    this.props.storeCertificate(certificate)
+    setTimeout(() => {
+      this.setState({ loading: false, visible: false });
+    }, 3000);
+  }
+
+  handleCancel = () => {
+    setTimeout(() => {
+      this.setState({ visible: false });
+    }, 10);
+  }
+  render() {
+    return(
+      <MainLayout>
+        <Row className="table-header">
+          <Col span={12}>
+            <h2 className="table-header__title">Summary</h2>
+          </Col>
+          <Col span={12} style={{ textAlign: 'right'}}>
+            <Button className="table-header__add-button" type="primary" size="large" onClick={this.showModal}>+ Certificate</Button>
+          </Col>
+        </Row>
+        <div className="table-menu__search-bar">
+          <Input placeholder="Search Certificate by email" size="large"/>
+        </div>
+        <Table
+          className="table"
+          columns={columns}
+          dataSource={this.props.certificates_list}
+          pagination={false}
+        />
+        <CreateCertificate
+          handleOk={this.handleOk}
+          handleCancel={this.handleCancel}
+          visible={this.state.visible}
+          loading={this.state.loading}
+        />
+      </MainLayout>
+    )
+  }
+}
+
+
+const mapStateToProps = (state) => ({
+  ...state.broker
+})
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    storeCertificate: (certificate) =>
+      dispatch({ type: 'CERTIFICATE_SUBMIT', payload: certificate })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BrokerIndex)
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,6 +4,7 @@ import NoMatch from '../layouts/no_match/NoMatch'
 import { Redirect } from 'react-router-dom';
 import Login from '../layouts/login/Login';
 import OwnerIndex from '../layouts/owner/index'
+import BrokerIndex from '../layouts/broker/index'
 
 class Routes extends Component {
   render() {
@@ -12,6 +13,7 @@ class Routes extends Component {
         <Route exact path="/" component={() => (<Redirect to={'/login'}/>)} />
         <Route path="/login" component={Login} />
         <Route path="/owner" component={OwnerIndex} />
+        <Route path="/broker" component={BrokerIndex} />
         <Route component={NoMatch} />
       </Switch>
     );
