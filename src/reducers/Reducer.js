384684a67ca0bd05328bc6b0e14a63cc1d578714
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-04-13 15:27:51 +0200

    Add broker reducer to state

--- src/reducers/Reducer.js
+++ src/reducers/Reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import owner from './owner'
+import broker from './broker'
 
 const Reducer = combineReducers({
   routing: routerReducer,
-  owner: owner
+  owner: owner,
+  broker: broker
 })
 
 export default Reducer
