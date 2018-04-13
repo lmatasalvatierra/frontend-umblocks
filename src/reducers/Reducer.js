0623799e6652ae1d7fff1eefc7671461b35e3216
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-04-13 16:00:06 +0200

    Add carrier reducer to state

--- src/reducers/Reducer.js
+++ src/reducers/Reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import owner from './owner'
 import broker from './broker'
+import carrier from './carrier'
 
 const Reducer = combineReducers({
   routing: routerReducer,
   owner: owner,
-  broker: broker
+  broker: broker,
+  carrier: carrier
 })
 
 export default Reducer
