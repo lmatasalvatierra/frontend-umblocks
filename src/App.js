f39d07a214b3dad291c50b76e198a03ff6d96c4e
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-04-16 12:36:08 +0200

    Add call of web3 setting

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { Provider } from 'react-redux';
 import { store, history } from './store/store';
 import { ConnectedRouter } from 'react-router-redux';
 import Routes from './routes/Routes'
+import getWeb3 from './util/getWeb3'
 import './App.css';
+
+getWeb3.then(results => {
+  console.log('Web3 initialized!')
+}).catch(() => {
+  console.log('Error in web3 initialization.')
+})
 
 class App extends Component {
   render() {
