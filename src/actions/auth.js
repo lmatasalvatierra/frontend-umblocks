53d30161faccca5031af260da6d7511b14f72a57
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-04-17 10:45:53 +0200

    Add functionality to redirect to user's homepage depending on user' type

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,6 +2,13 @@ import { store, history } from '../store/store'
 import COIManagerContract from '../build/contracts/COIManager.json'
 const contract = require('truffle-contract')
 
+function userLoggedIn(user) {
+  return {
+    type: 'USER_LOGGED_IN',
+    payload: user
+  }
+}
+
 export function loginUser(username, password) {
   let web3 = store.getState().web3.web3Instance
 
@@ -12,9 +19,14 @@ export function loginUser(username, password) {
 
       manager.deployed().then(function(instance) {
         // Attempt to sign up user.
-        instance.loginOwner.call(web3.fromAscii(username), web3.fromAscii(password)).then(function(result) {
-          if(result === true) {
-            history.push(`/owner`)
+        instance.login.call(web3.fromAscii(username), web3.fromAscii(password)).then(function(result) {
+          if(result.toNumber() === 0) {
+            dispatch(userLoggedIn({ 'username': username, 'is_authenticated': true, 'user_type': 'owner'}))
+            history.push('/owner')
+          }
+          else if(result.toNumber() === 1){
+            dispatch(userLoggedIn({ 'username': username, 'is_authenticated': true, 'user_type': 'carrier'}))
+            history.push('/carrier')
           }
         }).catch(function(result) {
           alert('Username does not exist');
--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,4 +1,7 @@
-export default (state = {}, action) => {
+const initialState = {
+  data: null
+}
+export default (state = initialState, action) => {
   switch (action.type) {
   case 'USER_LOGGED_IN':
     return {
