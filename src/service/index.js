883fe0b954be01e63c8c0db88f64db3d329aff6c
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-04-24 15:27:20 +0200

    Add authentication logic to new route in koa server

--- src/service/index.js
+++ src/service/index.js
@@ -4,7 +4,6 @@ import cors from 'kcors';
 import koaStatic from 'koa-static';
 import send from 'koa-send';
 import koaPassport from 'koa-passport';
-import Auth0Strategy from 'passport-auth0';
 import bodyParser from 'koa-bodyparser';
 import xmlParser from 'koa-xml-body';
 import session from 'koa-generic-session';
@@ -14,10 +13,13 @@ import dotenv from 'dotenv';
 import Web3Provider from './service/web3-service';
 import ManagerProvider from './service/manager-service';
 import appRouter from './appRouter';
+import authRouter from './route/authRouter';
 
 dotenv.config();
 
 const app = new Koa();
+
+app.use(bodyParser());
 
 // Initialize Web3
 const web3Instance = Web3Provider.instance;
@@ -25,13 +27,11 @@ const web3Instance = Web3Provider.instance;
 // Initialize Manager
 const managerInstance = ManagerProvider.instance(web3Instance);
 
-app.use(async ctx => {
-  await send(ctx, './build/index.html');
-});
+app.context.manager = managerInstance;
 
 // Initialize routes
 app.use(appRouter.routes());
-// app.use(authRouter.routes());
+app.use(authRouter.routes());
 
 const port = 3408;
 app.listen(port, () => {
--- src/service/route/authRouter.js
+++ src/service/route/authRouter.js
@@ -1,74 +1,42 @@
 import KoaRouter from 'koa-router';
-import koaPassport from 'koa-passport';
-import AuthCookieSession from '../core/authCookieSession';
+import Web3 from 'web3';
 
-/**
- * The `authRouter` instance implements the endpoints related with the
- * SSO authentication via auth0.
- */
 const authRouter = new KoaRouter();
 
-/**
- * The `/agent/login` endpoint is used for initiating the authentication
- * process via auth0. It will redirect the user to their domain and will
- * return to the `/agent/callback` endpoint including the parameters
- * needed for initializating the session.
- */
-authRouter.get(
-  'agent-login',
-  '/agent/login',
-  koaPassport.authenticate('auth0'),
-);
-
-/**
- * `/agent/logout` is used by the application to logout users.
- * In addition to the logout via passport-koa we will delete
- * the cookie `is_agent` that we use to determine if the user is
- * logged in across our different web applications.
- */
-authRouter.get('agent-logout', '/agent/logout', async ctx => {
-  ctx.logout();
-  AuthCookieSession.destroy(ctx);
-  if (ctx.session.quote) {
-    ctx.session.quote.authSession = {};
-  }
-  ctx.redirect('/');
-});
-
-/**
- * `/callback` is used as a redirect url once the `/agent/login` endpoint
- * process is completed and we receive a response redirect by auth0 that
- * contains the token that we'll use for initializing the user.
- * We are, in addition, going to create the `is_agent` cookie.
- */
-authRouter.get('agent-callback', '/callback', ctx => {
-  return koaPassport.authenticate('auth0', (err, user) => {
-    if (err) {
-      throw new Error(
-        'AuthRouter: Can not login after success authenticate callback.',
-      );
+authRouter.post('/api/v1/login', async ctx => {
+  try {
+    const manager = await ctx.manager.deployed();
+    const result = await manager.login(
+      Web3.utils.asciiToHex(ctx.request.body.username),
+      ctx.request.body.password
+    );
+    switch (result.toNumber()) {
+      case 0:
+        ctx.body = {
+          is_authenticated: true,
+          user_type: 'owner',
+          username: ctx.request.body.username,
+        };
+        break;
+      case 1:
+        ctx.body = {
+          is_authenticated: true,
+          user_type: 'carrier',
+          username: ctx.request.body.username,
+        };
+        break;
+      case 2:
+        ctx.body = {
+          is_authenticated: true,
+          user_type: 'broker',
+          username: ctx.request.body.username,
+        };
+        break;
+      default:
+        ctx.response.status = 403;
     }
-
-    ctx.login(user, () => {
-      if (ctx.isAuthenticated()) {
-        AuthCookieSession.create(ctx);
-      }
-
-      return ctx.redirect('/?agent_logged=1');
-    });
-  })(ctx);
-});
-
-/**
- * `/agent/session` tells us if there is an active session.
- */
-authRouter.get('/agent/session', ctx => {
-  if (ctx.isAuthenticated()) {
-    ctx.body = {
-      isAuthenticated: ctx.isAuthenticated(),
-      username: ctx.state.user.nickname,
-      email: ctx.state.user.displayName,
-    };
+  } catch (err) {
+    ctx.response.status = 403;
   }
 });
 
