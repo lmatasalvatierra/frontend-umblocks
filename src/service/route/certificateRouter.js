cce5fc048167b3b64ab514fc06419756243c5901
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-05-03 18:58:12 +0200

    Add endpoint to be able to post certificates to blockchain application

--- src/service/route/certificateRouter.js
+++ src/service/route/certificateRouter.js
@@ -1,20 +1,32 @@
 import KoaRouter from 'koa-router';
+import Web3 from 'web3';
 import logger from '../logger';
+
+const moment = require('moment');
 
 const certificateRouter = new KoaRouter();
 
-certificateRouter.get('/api/v1/certificate/:id', async ctx => {
+certificateRouter.post('/api/v1/certificate', async ctx => {
   try {
+    const policies = ctx.request.body.policies.map(policy => +policy);
     const manager = await ctx.manager.deployed();
-    const result = await manager.getPoliciesOfCoi(ctx.params.id);
-    const policies = JSON.parse(result);
-    if (policies.length > 0) {
-      ctx.response.body = policies;
-    } else {
-      ctx.throw('Generic Error', 500);
-    }
+    const result = await manager.createCoi(
+      Web3.utils.asciiToHex(ctx.request.body.owner_email),
+      parseInt(ctx.request.body.effective_date, 10),
+      policies
+    );
+    const certificate = {
+      owner_email: Web3.utils.hexToAscii(result.logs[0].args.ownerEmail),
+      owner_name: Web3.utils.hexToAscii(result.logs[0].args.ownerName),
+      effective_date: moment(
+        result.logs[0].args.effectiveDate.toNumber(),
+        'X',
+      ).format('DD/MM/YYYY'),
+      certificate_number: result.logs[0].args.certificateNumber.toNumber(),
+    };
+    ctx.response.body = certificate;
   } catch (err) {
-    logger.debug(err);
+    console.log(err);
     ctx.throw('Generic Error', 500);
   }
 });
