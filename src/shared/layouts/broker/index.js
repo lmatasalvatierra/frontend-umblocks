f8fbc3ce916d2d04672eee92a1f2092085c52c94
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-05-04 17:36:21 +0200

    Configure broker layout to be able to check certificate information

--- src/shared/layouts/broker/index.js
+++ src/shared/layouts/broker/index.js
@@ -4,50 +4,67 @@ import { Row, Col, Button, Table, Input } from 'antd';
 import { connect } from 'react-redux';
 import MainLayout from '../main/MainLayout';
 import CreateCertificate from '../../components/create_certificate/CreateCertificate';
-import { submittingCertificate } from '../../actions/certificates';
-
-const columns = [
-  {
-    title: 'Owner',
-    dataIndex: 'owner',
-    key: 'owner',
-    className: 'table__column',
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    key: 'email',
-    className: 'table__column',
-  },
-  {
-    title: 'Certificate Number',
-    dataIndex: 'certificate_number',
-    key: 'certificate_number',
-    className: 'table__column',
-  },
-  {
-    title: 'Effective Date',
-    dataIndex: 'effective_date',
-    key: 'effective_date',
-    className: 'table__column',
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (text, record) => (
-      <span>
-        <a href="#">View details</a>
-      </span>
-    ),
-    className: 'table__column',
-  },
-];
+import {
+  submittingCertificate,
+  viewingCertificate,
+} from '../../actions/certificates';
 
 class BrokerIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.columns = [
+      {
+        title: 'Owner',
+        dataIndex: 'owner',
+        key: 'owner',
+        className: 'table__column',
+      },
+      {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+        className: 'table__column',
+      },
+      {
+        title: 'Certificate Number',
+        dataIndex: 'certificate_number',
+        key: 'certificate_number',
+        className: 'table__column',
+      },
+      {
+        title: 'Effective Date',
+        dataIndex: 'effective_date',
+        key: 'effective_date',
+        className: 'table__column',
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <span>
+            <a
+              onClick={() =>
+                this.handleViewDetail(
+                  record.certificate_number,
+                  this.props.data.user_id,
+              )}
+            >
+              View details
+            </a>
+          </span>
+        ),
+        className: 'table__column',
+      },
+    ];
+  }
   state = {
     loading: false,
     visible: false,
   };
+
+  handleViewDetail = (certificateid, userid) => {
+    this.props.viewCertificate(certificateid, userid);
+  }
 
   showModal = () => {
     this.setState({
@@ -109,12 +126,15 @@ class BrokerIndex extends Component {
 
 const mapStateToProps = state => ({
   ...state.certificates,
+  ...state.auth,
 });
 
 const mapDispatchToProps = dispatch => {
   return {
     storeCertificate: certificate =>
       dispatch(submittingCertificate(certificate)),
+    viewCertificate: (certificateid, userid) =>
+      dispatch(viewingCertificate(certificateid, userid)),
   };
 };
 
