9786f637662a49d83ad048f5f45299167daa136c
Luis Mata Salvatierra <luis.mata.salvatierra@alumnos.upm.es>
2018-05-10 16:49:13 +0200

    Update cert routes to be able to send and retrieve uuid instead of cert_number

--- src/service/route/certificateRouter.js
+++ src/service/route/certificateRouter.js
@@ -3,6 +3,9 @@ import Web3 from 'web3';
 import logger from '../logger';
 
 const moment = require('moment');
+const uuidv4 = require('uuid/v4');
+const uuidToHex = require('uuid-to-hex');
+const hexToUuid = require('hex-to-uuid');
 
 const certificateRouter = new KoaRouter();
 
@@ -10,13 +13,17 @@ const STATUS = ['Active', 'Cancelled', 'Expired'];
 
 certificateRouter.post('/api/v1/certificate', async ctx => {
   try {
-    const policies = ctx.request.body.policies.map(policy => +policy);
+    const certificateUUID = uuidToHex(uuidv4(), true);
+    const policies = ctx.request.body.policies.map(policy =>
+      uuidToHex(policy, true)
+    );
     const manager = await ctx.manager.deployed();
     const result = await manager.createCoi(
       Web3.utils.asciiToHex(ctx.request.body.owner_email),
       parseInt(ctx.request.body.effective_date, 10),
       parseInt(ctx.request.body.user_id, 10),
-      policies
+      policies,
+      certificateUUID
     );
     const certificate = {
       owner_email: Web3.utils.hexToAscii(result.logs[0].args.ownerEmail),
@@ -25,7 +32,7 @@ certificateRouter.post('/api/v1/certificate', async ctx => {
         result.logs[0].args.effectiveDate.toNumber(),
         'X',
       ).format('DD/MM/YYYY'),
-      certificate_number: result.logs[0].args.certificateNumber.toNumber(),
+      certificate_number: hexToUuid(result.logs[0].args.certificateUUID),
     };
     ctx.response.body = certificate;
   } catch (err) {
@@ -37,11 +44,12 @@ certificateRouter.post('/api/v1/certificate', async ctx => {
 certificateRouter.get('/api/v1/certificate/:id', async ctx => {
   try {
     const manager = await ctx.manager.deployed();
-    const result = await manager.getCoi(ctx.params.id);
+    const uuid = uuidToHex(ctx.params.id, true);
+    const result = await manager.getCoi(uuid);
     const jsonPolicies = JSON.parse(result[4]);
     const policies = jsonPolicies.map(policy => {
       return {
-        policy_number: policy.policy_number,
+        policy_number: hexToUuid(policy.policy_uuid),
         insurance_type: policy.insurance_type,
         effective_date: moment(parseInt(policy.effective_date, 10), 'X').format(
           'DD/MM/YYYY',
@@ -54,7 +62,7 @@ certificateRouter.get('/api/v1/certificate/:id', async ctx => {
       };
     });
     const certificate = {
-      certificate_number: result[0].toNumber(),
+      certificate_number: hexToUuid(result[0]),
       owner_email: Web3.utils.hexToAscii(result[1]),
       owner_name: Web3.utils.hexToAscii(result[2]),
       effective_date: moment(result[3].toNumber(), 'X').format('DD/MM/YYYY'),
